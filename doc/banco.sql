a253161bc8fc1887076461937978266da9ae438a
Blabos de Blebe <blabos@blabos.org>
2010-09-01 01:33:50 -0300

    Alterações na definição dos dados
    Baseado na documentação oficial do sqlite, fiz algumas alterações
    de tipos.
    
    Alterei também alguns nomes de colunas para torná-los mais
    amigáveis

---
+++ doc/banco.sql
@@ -0,0 +1,22 @@
+-- pragma foreign_keys = on;
+-- esta pragma precisa ser habilitada manualmente no banco
+
+create table authors (
+    author_id       integer primary key autoincrement,
+    username        text not null,
+    password        text not null,
+    email           text not null,
+    full_name       text not null
+);
+
+create table posts (
+    post_id         integer primary key autoincrement,
+    author_id       integer not null,
+    publish_date    integer not null,
+    title           text not null,
+    content         text not null,
+    status          integer not null default 1,
+    foreign key (author_id) references authors(author_id)
+);
+
+-- Post status: 1 -> Created, 2 -> Deleted, 3 -> Published
